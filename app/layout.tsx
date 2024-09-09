53e271f706097b80de1975df2e1a1f3b147bd399
unknown <viniciusreis3105@gmail.com>
2024-09-09 10:15:47 -0300

    chore: add shadcn-ui setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={}>{children}</body>
+    <html lang="pt-br" className="dark">
+      <body
+        className={cn("min-h-screen bg-background antialiased", inter.variable)}
+      >
+        {children}
+      </body>
     </html>
   );
 }
